1f0af8743d7683f97f22d0f39aad30dceeaa9912
Shawn Inder <shawn@vigour.io>
2016-03-15 12:08:38 +0100

    Add a space before the `*`s

--- test/commentfactory.js
+++ test/commentfactory.js
@@ -2,13 +2,13 @@
 
 module.exports = exports = function (fnName, id) {
   return `/**
-* @id ${id}
-* @function ${fnName}
-* ***Oh*** man this *function* does **so** many [things](http://things.com)!!!
-* param {string} text - Words, \`sentences\`, and more*
-* param {number} amount
-* param {boolean} isCool - always \`true\`
-* param {array} things All sorts of things
-* returns \`undefined\`, \`void 0\`, \`this.meh\`, you name it!
-*/`
+ * @id ${id}
+ * @function ${fnName}
+ * ***Oh*** man this *function* does **so** many [things](http://things.com)!!!
+ * @param {string} text - Words, \`sentences\`, and more*
+ * @param {number} amount
+ * @param {boolean} isCool - always \`true\`
+ * @param {array} things All sorts of things
+ * @returns \`undefined\`, \`void 0\`, \`this.meh\`, you name it!
+ */`
 }
