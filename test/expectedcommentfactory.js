c5d1f8c33acff8d37e167237cfa5e2e735964a3e
Shawn Inder <shawn@vigour.io>
2016-04-03 15:53:42 +0200

    new tests for new jsdoc2md

--- test/expectedcommentfactory.js
+++ test/expectedcommentfactory.js
@@ -3,9 +3,9 @@ module.exports = exports = function expectedCommentFactory (fnName, id) {
   return `#### var canMakeYouHappy = ${fnName}(text, amount, isCool, things)
 
 ***Oh*** man this *function* does **so** many [things](http://things.com)!!!
-- **param** {*string*} text - Words, \`sentences\`, and more*
-- **param** {*number*} amount
-- **param** {*boolean*} isCool - always \`true\`
-- **param** {*array*} things All sorts of things
-- **returns** {*boolean*} canMakeYouHappy - usually \`true\``
+- **text** (*string*) - Words, \`sentences\`, and more*
+- **amount** (*number*)
+- **isCool** (*boolean*) - always \`true\`
+- **things** (*array*) - All sorts of things
+- **returns** (*boolean*) canMakeYouHappy - usually \`true\``
 }
